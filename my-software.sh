87e3423b29009e301caca852b1e0698670ce3c04
ahmad-Wattar <74015246+ahmad-Wattar@users.noreply.github.com>
2021-01-04 14:03:30 +0100

    Update my-software.sh

--- my-software.sh
+++ my-software.sh
@@ -1,10 +1,25 @@
 while getopts i:o: flag
-do
-    case "${flag}" in
-        i) input_dir=${OPTARG};;
-        o) output_dir=${OPTARG};;
-    esac
-done
+
+if [-n "$input_dir$"] then
+	do
+		case "${flag}" in
+			i) input_dir=${OPTARG};;
+			o) output_dir=${OPTARG};;
+		esac
+	done
 
 
-echo $(python3 indexing_full.ipynb $input_dir $output_dir) > /dev/null
+	echo $(python3 indexing_full.ipynb $input_dir $output_dir) > /dev/null
+
+else
+    do
+        case "${flag}" in
+            i) input_dir=${OPTARG};;
+            o) output_dir=${OPTARG};;
+        esac
+    done
+    echo $(python3 indexing_full.ipynb $input_dir $output_dir) > /dev/null
+    
+fi
+
+

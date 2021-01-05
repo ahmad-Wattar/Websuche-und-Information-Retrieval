8e353d870dce89d3386068ff31d65eaa27c2b607
acmvs <alexander.hesse@student.uni-halle.de>
2021-01-05 09:35:51 +0100

    remove empty rows in .sh files

--- eval.sh
+++ eval.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 pip install pytrec_eval
-
 python evaluation.py "results/run.txt" "touche2020-task2-relevance-withbaseline(1).qrels"
--- my-software.sh
+++ my-software.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
-
 input_dir="data/docs"
 output_dir="results"
-
 while getopts ":i::o:" opt; do
   case ${opt} in
     i )
@@ -20,8 +17,5 @@ done
 shift $((OPTIND -1))
 echo $input_dir
 echo $output_dir
-
 pip install elasticsearch
-
 python indexing_full.py $input_dir $output_dir
-    

16fb9d03f04f46f30b1de7b8751d98cb15eb70bd
ajdyd <ekaterina.shirshakova@student.uni-halle.de>
2021-02-02 10:40:57 +0100

    experiments

--- eval.sh
+++ eval.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip install pytrec_eval
-python evaluation.py "results/run.txt" "touche2020-task2-relevance-withbaseline(1).qrels"
+python evaluation.py "results/run_test_t_lem.txt" "touche2020-task2-relevance-withbaseline(1).qrels"
